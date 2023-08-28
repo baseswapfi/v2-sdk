efca8f8d73d65c2e930f078ae1143995f599a42e
0xshoegazer <0xshoegazer@gmail.com>
2023-08-28 19:00:44 -0400

    versions

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,6 @@ import JSBI from 'jsbi';
 
 export const V2_FACTORY_ADDRESS = '0xFDa619b6d20975be80A10332cD39b9a4b0FAa8BB';
 
-// .... I dont know
 export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000);
