5684334529bd0cc36be2869fe706a0cff8ca12bf
0xshoegazer <0xshoegazer@gmail.com>
2023-10-20 16:03:10 -0400

    v2 router

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const V2_FACTORY_ADDRESS = '0xFDa619b6d20975be80A10332cD39b9a4b0FAa8BB';
 
 export const V2_FACTORY_ADDRESSES: { [chainId in ChainId]?: string } = {
   [ChainId.BASE]: V2_FACTORY_ADDRESS,
-  [ChainId.SCROLL]: '',
+  [ChainId.SCROLL]: '0x74a52eb08d699CD8BE1d42dA4B241d526B8a8285',
 };
 
 // export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
@@ -13,7 +13,7 @@ export const INIT_CODE_HASH = '0xb618a2730fae167f5f8ac7bd659dd8436d571872655bcb6
 
 export const INIT_CODE_HASHES: { [chainId in ChainId]?: string } = {
   [ChainId.BASE]: INIT_CODE_HASH,
-  [ChainId.SCROLL]: '',
+  [ChainId.SCROLL]: INIT_CODE_HASH,
 };
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000);
